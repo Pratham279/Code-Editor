5b3529f7a19e5977e99057b9626c83575244f32b
Pratham279 <prathammalavia9@gmail.com>
2025-06-07 00:10:58 +0530

    Completion of Project : Changes made in files: index.html App.css App.jsx index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
+import Editor from "./components/Editor";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [html, setHtml] = useState('<!-- Write your HTML here -->');
+  const [css, setCss] = useState('/* Write your CSS here */');
+  const [js, setJs] = useState('// Write your JavaScript here');
+  const [srcDoc, setSrcDoc] = useState('');
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250);
+    
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <div className="pane top-pane">
+        <Editor 
+          language="html" 
+          displayName="HTML" 
+          value={html} 
+          onChange={setHtml}
+        />
+        <Editor 
+          language="css" 
+          displayName="CSS" 
+          value={css} 
+          onChange={setCss}
+        />
+        <Editor 
+          language="javascript" 
+          displayName="JS" 
+          value={js} 
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane bottom-pane borderless-frame">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   )
 }
 
