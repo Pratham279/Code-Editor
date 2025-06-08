62e5e7c050dffc55633ebac95ae5a09f99562228
Pratham279 <prathammalavia9@gmail.com>
2025-06-08 17:58:16 +0530

    Improvements Made: Made vertical and Horizontal Panels Resizable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Editor from "./components/Editor";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import './App.css'
 
 function App() {
@@ -23,37 +24,56 @@ function App() {
   }, [html, css, js]);
 
   return (
-    <>
-      <div className="pane top-pane">
-        <Editor 
-          language="html" 
-          displayName="HTML" 
-          value={html} 
-          onChange={setHtml}
-        />
-        <Editor 
-          language="css" 
-          displayName="CSS" 
-          value={css} 
-          onChange={setCss}
-        />
-        <Editor 
-          language="javascript" 
-          displayName="JS" 
-          value={js} 
-          onChange={setJs}
-        />
-      </div>
-      <div className="pane bottom-pane borderless-frame">
-        <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          width="100%"
-          height="100%"
-        />
-      </div>
-    </>
+    <div className="app-container">
+      <PanelGroup direction="vertical">
+        <Panel defaultSize={50} minSize={20} maxSize={75}>
+          {/* The .pane div should fill this Panel */}
+          <div className="pane top-pane"> 
+            <PanelGroup direction="horizontal">
+              <Panel defaultSize={30} minSize={20}>
+                <Editor 
+                  language="html" 
+                  displayName="HTML" 
+                  value={html} 
+                  onChange={setHtml}
+                />
+              </Panel>
+              <PanelResizeHandle className="resize-handle-vertical" />
+              <Panel defaultSize={40} minSize={30}> {/* Explicitly set defaultSize */}
+                <Editor 
+                  language="css" 
+                  displayName="CSS" 
+                  value={css} 
+                  onChange={setCss}
+                />
+              </Panel>
+              <PanelResizeHandle className="resize-handle-vertical" />
+              <Panel defaultSize={30} minSize={20}>
+                <Editor 
+                  language="javascript" 
+                  displayName="JS" 
+                  value={js} 
+                  onChange={setJs}
+                />
+              </Panel>
+            </PanelGroup>
+          </div>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-horizontal" />
+        <Panel defaultSize={50} minSize={20} maxSize={75}>
+          {/* The .pane div fills this Panel */}
+          <div className="pane bottom-pane borderless-frame"> 
+            <iframe
+              srcDoc={srcDoc}
+              title="output"
+              sandbox="allow-scripts"
+              width="100%"
+              height="100%"
+            />
+          </div>
+        </Panel>
+      </PanelGroup>
+    </div>
   )
 }
 
